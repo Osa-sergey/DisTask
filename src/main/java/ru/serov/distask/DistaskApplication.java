a10b79aa09e8c0e039c3b9aabc1ddca0556f71e5
Serg Serov <owchin.s@yandex.ru>
2022-12-01 21:27:14 +0300

    init commit

---
+++ src/main/java/ru/serov/distask/DistaskApplication.java
@@ -0,0 +1,13 @@
+package ru.serov.distask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/serov/distask/DistaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serov.distask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
