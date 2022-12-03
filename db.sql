b93fe7e7dbee3bfe619d9c3854f9288c19e27d25
Serg Serov <owchin.s@yandex.ru>
2022-12-03 21:09:44 +0300

    add indexes to db and change primary key type. Delete unique for productid

--- db.sql
+++ db.sql
@@ -1,22 +1,21 @@
-create schema company;
+create schema if not exists company;
 
-set schema 'company';
-
-    create table company.products
-    (
-        id             serial primary key,
-        name           varchar(200) not null unique,
-        description    text         not null,
-        implement_cost money        not null
-    );
-
-create table company.articles
+create table if not exists company.products
 (
-    id          serial
-        primary key,
-    productid   integer            not null unique
+    id             bigserial primary key,
+    name           varchar(200) not null unique,
+    description    text         not null,
+    implement_cost money        not null
+);
+
+create table if not exists company.articles
+(
+    id          bigserial primary key,
+    productid   integer            not null
         references company.products,
-    name        varchar(200)       not null,
+    name        varchar(200)       not null unique,
     content     text               not null,
     create_date date default now() not null
 );
+
+create index if not exists product_id_index ON company.articles (productid);
