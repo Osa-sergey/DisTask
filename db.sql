5c46f36bd25ea87f5e647e2cf44160fbcbcc6e0d
Serg Serov <owchin.s@yandex.ru>
2022-12-05 14:25:08 +0300

    change type and name for productid

--- db.sql
+++ db.sql
@@ -11,11 +11,11 @@ create table if not exists company.products
 create table if not exists company.articles
 (
     id          bigserial primary key,
-    productid   integer            not null
+    product_id  bigint             not null
         references company.products,
     name        varchar(200)       not null unique,
     content     text               not null,
     create_date date default now() not null
 );
 
-create index if not exists product_id_index ON company.articles (productid);
+create index if not exists product_id_index ON company.articles (product_id);
